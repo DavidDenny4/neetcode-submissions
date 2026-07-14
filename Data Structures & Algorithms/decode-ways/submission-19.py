2187173b02f00f9f3965962fb7aae327e78ecfdc
David Denny <85903997+DavidDenny4@users.noreply.github.com>
2026-07-13 23:50:58 -0400

    Add: decode-ways - submission-19

---
+++ Data Structures & Algorithms/decode-ways/submission-19.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        vals = [int(c) for c in s]
+        dp = [None] * (len(s) + 1)
+
+        def dfs(index):
+
+            if dp[index] is not None:
+                return dp[index]
+
+            if index == len(s):
+                return 1
+            
+            if vals[index] == 0:
+                return 0
+            
+            count = 0
+            count += dfs(index + 1)
+            if ((index + 1) < len(s) and vals[index] in [1,2] 
+            and vals[index + 1] in [1,2,3,4,5,6]):
+                count += dfs(index + 2)
+
+            dp[index] = count
+            return count             
+        
+        return dfs(0)
