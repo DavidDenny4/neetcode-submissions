00a7f27423ae611df26dc0bd8e1cec908c22757a
David Denny <85903997+DavidDenny4@users.noreply.github.com>
2026-07-14 20:50:44 -0400

    Add: maximum-sum-circular-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        max_sum = 0
+        prev_sum = 0
+        print(nums)
+        for R in range(2 * len(nums) - 1):
+            print(prev_sum)
+            prev_sum = max(prev_sum, 0)
+            prev_sum += nums[R % len(nums)]
+            print(f"added {nums[R % len(nums)]} prev sum now {prev_sum}")
+            max_sum = max(max_sum, prev_sum)
+        return max_sum
