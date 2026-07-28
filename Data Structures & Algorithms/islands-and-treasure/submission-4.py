e93d6ffe2391da8944f69aacb20b276f793f7f18
David Denny <85903997+DavidDenny4@users.noreply.github.com>
2026-07-28 17:40:11 -0400

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        visited = set()
+        
+        def dfs(r, c, dist):
+
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or grid[r][c] == -1:
+                return
+            
+            visited.add((r, c))
+            if grid[r][c] == 2147483647:
+                grid[r][c] = dist
+            
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc, dist + 1)
+            
+            visited.remove((r, c))
+            
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    dfs(r, c, 0)
+
+        
+            
+            
+
+            
+
+
